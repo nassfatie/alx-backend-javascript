78cbc10ccc7dca13ea57e4d015c70c7388cd7d73
nassfatie <nassfatie@gmail.com>
2023-06-30 00:31:11 +0300

    upated task 2

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,4 @@
-export function taskFirst() {
+export default function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
-/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
   }
 
   return [task, task2];
