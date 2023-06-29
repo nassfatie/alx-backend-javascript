d6eddc403e226175f9b8a0dd5caa5bce3cc79867
nassfatie <nassfatie@gmail.com>
2023-06-30 00:26:16 +0300

    task 2

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-var */
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
+}
