fffb722cf360486d0fde526f1450ee8bba2c7304
nassfatie <nassfatie@gmail.com>
2023-07-08 01:00:19 +0300

    config files

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -11,6 +11,6 @@ const row: RowElement = {
 const newRowID: RowID = CRUD.insertRow(row);
 const updateRow: RowElement = {...row, age: 23}
 
-CRUD.updateRow(newRowID, updatedRow);
+CRUD.updateRow(newRowID, updateRow);
 
-CRUD.deleteRow(newRowID);
+CRUD.deletetRow(newRowID);
