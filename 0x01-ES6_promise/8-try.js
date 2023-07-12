a462785b900a1c682f554441137f5c22c7d97368
nassfatie <nassfatie@gmail.com>
2023-07-12 17:54:41 +0300

    task 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-    if (denominator === 0) {
-      // eslint-disable-next-line no-throw-literal
-      throw 'cannot divide by 0';
-    } else {
-      return (numerator / denominator);
-    }
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return (numerator / denominator);
 }
