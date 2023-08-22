a80efd6c404edfb9622b9cae55e36fea4207dc0e
nassfatie <nassfatie@gmail.com>
2023-08-22 23:35:27 +0300

    updated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,9 @@
-function displayMessage(data) {
-    console.log(data);
-  }
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
   
   module.exports = displayMessage;
